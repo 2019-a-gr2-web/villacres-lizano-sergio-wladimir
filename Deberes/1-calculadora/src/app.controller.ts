ffd96bfca5395e70f8eccdde9bd6ec21c96929dd
Sergio Villacres <sergiovillacres3@gmail.com>
2019-04-21 19:29:08 -0500

    Deber 1- Calculadora
    Suma
    Resta
    Producto
    Division

---
+++ Deberes/1-calculadora/src/app.controller.ts
@@ -0,0 +1,63 @@
+import { Request,Response, Controller, Get, Headers, Post, Body, Put, Query, Delete } from '@nestjs/common';
+import { AppService } from './app.service';
+import { response } from 'express';
+
+@Controller('/api')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('/suma')
+  suma(@Headers() header,@Response() respuesta) {
+    
+    if(header.numero1!=null && header.numero2!=null){
+      const resultado = Number(header.numero1) + Number(header.numero2);
+      console.log("Resultado de la suma es: "+resultado);
+      return respuesta.status(200).send('La respuesta es: '+resultado);
+    }
+    else{
+      return respuesta.status(400).send('Error de parametros');
+    }
+    
+  }
+  @Post('/resta')
+  resta(@Body() body,@Response() respuesta){
+    if(!isNaN(body.numero1) && !isNaN(body.numero2)){
+      const resultado = Number(body.numero1) - Number(body.numero2);
+      console.log("Resultado de la resta es: "+resultado);
+      return respuesta.status(201).send('La respuesta es: '+resultado);
+    }
+    else{
+      return respuesta.status(401).send('Error de parametros');
+    }
+  }
+  @Put('/producto')
+  producto(@Query() query,@Response() respuesta){
+    if(!isNaN(query.numero1) && !isNaN(query.numero2)){
+      const resultado = Number(query.numero1) * Number(query.numero2);
+      console.log("Resultado de la multiplicacion es: "+resultado);
+      return respuesta.status(202).send('La respuesta es: '+resultado);
+    }
+    else{
+      return respuesta.status(402).send('Error de parametros');
+    }
+  }
+  @Delete('/division')
+  division(@Query() query, @Body() body,@Headers() header,@Response() respuesta){
+    if(isNaN(query.numero) && !isNaN(body.numero) && header.numero!=null){
+      const resultado = Number(body.numero)/Number(header.numero);
+      return respuesta.status(203).send('La respuesta es: '+ resultado)
+    }
+    else if(isNaN(body.numero) && !isNaN(query.numero) && header.numero!=null){
+      const resultado = Number(query.numero)/Number(header.numero);
+      return respuesta.status(203).send('La respuesta es: '+ resultado)
+    }
+    else if(header.numero==null && !isNaN(query.numero) && !isNaN(body.numero)){
+      const resultado = Number(query.numero)/Number(body.numero);
+      return respuesta.status(203).send('La respuesta es: '+ resultado)
+    }
+    else{
+      return respuesta.status(403).send('error')
+    }
+  }
+
+}
