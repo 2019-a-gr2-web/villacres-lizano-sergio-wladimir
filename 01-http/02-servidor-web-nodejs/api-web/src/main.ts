7224102163d1b8b1914173512d54d3bec0eae37b
Sergio Villacres <sergiovillacres3@gmail.com>
2019-04-24 15:52:43 -0500

    Views, Funciones, Operadores de funciones

--- 01-http/02-servidor-web-nodejs/api-web/src/main.ts
+++ 01-http/02-servidor-web-nodejs/api-web/src/main.ts
@@ -5,6 +5,8 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   app.use(cookieParcer());
-  await app.listen(3001);
+  //@ts-ignore
+  app.set('view engine','ejs');
+  await app.listen(3000);
 }
 bootstrap();
